7647a561285a75a6299dc67e5faedad787adebd9
Chloé Tan <chloe@MacBook-Pro-de-Chloe.local>
2024-03-21 17:25:13 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,79 +9,92 @@ function App() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [show, setShow] = useState(true);
+  const [border, setBorder] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (password !== confirmPassword) {
       alert("Vos deux mots de passe ne sont pas identiques");
+      setBorder(false);
     }
   };
 
   return (
     <>
       <main className="container">
-        <form onSubmit={handleSubmit}>
-          <h1>Create account</h1>
-          <p>Name</p>
-          <input
-            value={name}
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Jean Dupont"
-            onChange={(event) => {
-              setName(event.target.value);
-            }}
-          />
-          <p>Email</p>
-          <input
-            value={email}
-            type="email"
-            name="email"
-            id="email"
-            placeholder="E-mail"
-            onChange={(event) => {
-              setEmail(event.target.email);
-            }}
-          />
-          <p>Password</p>
-          <input
-            value={password}
-            type="password"
-            name="password"
-            id="password"
-            placeholder="AzErTy1234!"
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-          />
-          <p>Confirm your password</p>
-          <input
-            value={confirmPassword}
-            type="password"
-            name="confirm-password"
-            id="confirm-password"
-            placeholder="AzErTy1234!"
-            onChange={(event) => {
-              setConfirmPassword(event.target.value);
-            }}
-          />
-          <button
-            className={show === true ? "results" : "form"}
-            onClick={() => {
-              setShow(true);
-              <div className="StepTwo">
-                <h1>Results</h1>
-                <span>Name: {name}</span>
-                <span>Email: {email}</span>
-                <span>Password: {password}</span>
-                <button>Edit your information</button>
-              </div>;
-            }}
-          >
-            Register
-          </button>
-        </form>
+        {show !== false ? (
+          <form onSubmit={handleSubmit}>
+            <h1>Create account</h1>
+            <p>Name</p>
+            <input
+              value={name}
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Jean Dupont"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
+            <p>Email</p>
+            <input
+              className={border === true && "red"}
+              value={email}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="E-mail"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+            <p>Password</p>
+            <input
+              value={password}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="AzErTy1234!"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <p>Confirm your password</p>
+            <input
+              value={confirmPassword}
+              type="password"
+              name="confirm-password"
+              id="confirm-password"
+              placeholder="AzErTy1234!"
+              onChange={(event) => {
+                setConfirmPassword(event.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                setShow(false);
+              }}
+            >
+              Register
+            </button>
+          </form>
+        ) : (
+          <div className="StepTwo">
+            <h1>Results</h1>
+            <div className="sumUp">
+              <span>Name: {name}</span>
+              <span>Email: {email}</span>
+              <span>Password: {password}</span>
+            </div>
+            <button
+              onClick={() => {
+                setShow(true);
+              }}
+            >
+              Edit your information
+            </button>
+          </div>
+        )}
       </main>
       <Footer />
     </>
